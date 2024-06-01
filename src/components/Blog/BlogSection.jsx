42aab719f13ad02c40b29716d7512334fe3bae9a
aounjabbarhashmi <compilerv6@gmail.com>
2024-06-01 19:08:25 +0500

    commit

--- src/components/Blog/BlogSection.jsx
+++ src/components/Blog/BlogSection.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from "react";
+
 function BlogSection() {
   const data = [
     {
@@ -19,7 +21,36 @@ function BlogSection() {
       content: "",
       img: "./images/img3.png",
     },
+    {
+      date: "February - 22th 2024",
+      heading: "Will you be attending Affiliate Summit Europe?",
+      content:
+        "I have been a loyal customer of this auto parts company for years and I cannot recommend them enough. Their extensive selection.",
+      img: "./images/img2.png",
+    },
+    {
+      date: "February - 22th 2024",
+      heading: "Will you be attending Affiliate Summit Europe?",
+      content: "",
+      img: "./images/img3.png",
+    },
+    {
+      date: "February - 22th 2024",
+      heading: "Will you be attending Affiliate Summit Europe?",
+      content:
+        "I have been a loyal customer of this auto parts company for years and I cannot recommend them enough. Their extensive selection.",
+      img: "./images/img2.png",
+    },
   ];
+  const [scroll, setScroll] = useState(0);
+  const scrollDiv = useRef(null);
+  useEffect(() => {
+    console.log(scroll);
+    scrollDiv.current.scrollTo({
+      left: scroll,
+      behavior: "smooth",
+    });
+  }, [scroll]);
   return (
     <>
       <div className="text-white mt-[350px] max-[500px]:mt-[400px]">
@@ -33,12 +64,23 @@ function BlogSection() {
         </div>
 
         <div className="flex justify-between  p-12 items-center gap-6">
-          <img src="./images/Chevron--left.png" alt="" />
-          <div className="flex  text-left max-w-[1200px] w-full overflow-x-auto items-start gap-6">
+          <img
+            src="./images/Chevron--left.png"
+            alt=""
+            onClick={() => {
+              if (scroll != 0) {
+                setScroll(scroll - 300);
+              }
+            }}
+          />
+          <div
+            ref={scrollDiv}
+            className="flex text-left max-w-[1200px] w-full overflow-x-auto items-start gap-6"
+          >
             {data.map((item) => {
               return (
                 <>
-                  <div className="min-w-[300px]  flex-col flex p-4  gap-8 scale-90 transition-all duration-500 ease-in-out ">
+                  <div className="min-w-[300px] max-w-[350px]  flex-col flex p-4  gap-8 scale-90 transition-all duration-500 ease-in-out ">
                     <time
                       dateTime="January - 06th 2024 "
                       className="text-[2rem] text-[#B0B0B0] font-[400]"
@@ -57,7 +99,14 @@ function BlogSection() {
           </div>
           <img
             src="./images/Chevron--left.png"
-            onClick={() => {}}
+            onClick={() => {
+              if (
+                scroll <
+                Math.floor(scrollDiv.current.scrollWidth / 100) * 100
+              ) {
+                setScroll(scroll + 300);
+              }
+            }}
             className="rotate-180"
             alt=""
           />
